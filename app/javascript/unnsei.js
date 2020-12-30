fbafc2f6494ec68776f1648b730a3074a310a606
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-12-31 05:55:18 +0900

    viewの修正

--- app/javascript/unnsei.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-const btn = document.getElementById('btn')
-const unnsei = document.getElementById('unnsei')
-const results = ['大吉です','中吉です','吉です','小吉です','凶です'];
-btn.addEventListener('click', function(){
-unnsei.textContent = results[Math.floor(Math.random()*results.length)];
-});
-}
