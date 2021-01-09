fe2d7bc3a9ff05756304c9ce758de0f998e8b536
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-01-09 14:25:57 +0900

    ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: "questions#index"
-  resources :questions, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :questions, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     collection do
       get 'search'
     end
