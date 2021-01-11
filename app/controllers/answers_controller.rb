39f3442fadec549ef77749e774247d7da9333f2a
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-01-11 15:34:17 +0900

    コードの修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -17,11 +17,9 @@ class AnswersController < ApplicationController
   end
 
   def edit
-    @answer = Answer.find(params[:id])
   end
 
   def update
-    @answer = Answer.find(params[:id])
     if @answer.update(answer_params)
       redirect_to question_path(@question.id)
     else
@@ -30,7 +28,6 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @answer = Answer.find(params[:id])
     @answer.destroy
     redirect_to root_path
   end
