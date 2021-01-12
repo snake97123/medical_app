0d4cb5096082bdffcbdbf476e10c71ae785a2a69
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-01-12 11:09:58 +0900

    controllerの編集

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
-    @questions = Question.all
+    @questions = Question.page(params[:page]).per(10).order('updated_at DESC')
     @posts = Question.all.count
   end
 
