aaf968f9e4f7be23b7da63b1d51799aacc1ae40f
snake97123 <73522198+snake97123@users.noreply.github.com>
2021-01-15 07:12:35 +0900

    マイグレーションファイルの修正

--- db/migrate/20210114220234_create_likes.rb
+++ db/migrate/20210114220234_create_likes.rb
@@ -2,10 +2,10 @@ class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
       t.references :user
-      t.references :question
+      t.references :answer
 
       t.timestamps
-      t.index [:user_id, :question_id], unique: true
+      t.index [:user_id, :answer_id], unique: true
     end
   end
 end
